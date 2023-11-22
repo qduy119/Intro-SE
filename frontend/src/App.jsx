f91dd4141bb41f729a4ebdd3efa875f2fb6af8e8
21120440 <duyialypro@gmail.com>
2023-11-22 16:14:48 +0700

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Layout from "./components/Layout/Layout";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+    return (
+        <Layout>
+            <Navbar />
+            <Header />
+            <Footer />
+        </Layout>
+    );
+}
+
+export default App;
